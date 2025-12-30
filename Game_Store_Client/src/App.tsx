8c7d5743f15cea090d1ab18a90153e0d42af0531
Emilis Kaupas <Asemilis123@gmail.com>
2025-12-30 17:30:06 +0200

    Added 404 NotFound Page, fixed UI

--- Game_Store_Client/src/App.tsx
+++ Game_Store_Client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GameList from './pages/GameList';
 import GameDetail from './pages/GameDetail';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<GameList />} />
           <Route path="/game/:id" element={<GameDetail />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
